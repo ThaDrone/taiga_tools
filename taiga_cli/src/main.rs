bee5c5ab2a49ef60bb35a44290d8c288cd33f678
Martijn van Noord (mint) <martijn@vannoord.nu>
2023-01-28 18:34:34 +0100

    added code to initialize program

--- taiga_cli/src/main.rs
+++ taiga_cli/src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-use cli_storage::{LocalStorage, Session};
+use cli_storage::{LocalStorage, Session, Config};
 use taiga_lib::lib_routes::{self, TaigaRoute, AuthType, Authentificate};
 
 mod cli_storage;
@@ -22,26 +22,31 @@ pub fn main(){
 Initializing the program. It will try to read config and session data from files.
 If they are not present, it will go trough a "wizard" where the login is asked and which project should be used.
 */
-fn initialize() -> Option<()>{
+fn initialize() -> Option<(Session, Config)>{
 
-    let session = match  cli_storage::Session::load(){
-        Ok(session) => {
-            let mut session = session;
-        }
+    let mut session = match  cli_storage::Session::load(){
+        Ok(session) => session,
         Err(e) => {
-    
             println!("{e}");
-            login();
+            login()
         }
     };
 
-    println!("### test ");
+    let config = match Config::load(){
+        Ok(config) => config,
+        Err(e) => {
+            println!("{e}");
+            config()
+        },
+    }
 
-    Some(()) 
+    Some((session,config )) 
 }
 
 /// This function will prompt the user to login. 
 fn login() -> Session{
+
+    // TODO #6 Check for environment variables in the login function
 
 
     let mut username=  String::new();
@@ -59,27 +64,45 @@ fn login() -> Session{
     let username =  username.trim().to_string();
     let password = password.trim().to_string();
 
-    println!("username:  {}",username);
-    println!("password:  {}",password);
-
+    // TODO Based on some options we should have different types here (Github etc)
+    // Parsing the recieved username and password into a Authtype::Taiga route
     let auth_type = lib_routes::AuthType::Taiga{
         username,
         password,
     }; 
 
-
-    if let AuthType::Taiga { username, password } = &auth_type {
-        println!("username:  {}",username);
-        println!("password:  {}",password);
-    }
+    // Creating a route
     let route = lib_routes::Authentificate{auth_type:&auth_type};
+
+    // Running the route
+    // TODO handle this properly
     let data = route.request(&BASE_URL.to_owned(), &None).expect("Could not login: ");
 
-    let auth_key = data["auth_key"].to_string().to_owned();
-    Session {auth_key}
+    let auth_key = data["auth_key"].to_string();
+    let id = data["id"].to_string();
+
+    Session {auth_key,id}
 
     }
 
+fn config() -> Config{
+
+    
+    let config = Config::load();
+
+    if let Ok(config) = config {
+        return config;
+    };
+
+    todo!(); // Create a wizard for creating config files
+    // TODO #5 
+    // Get all projects for the user
+    // display the projects
+    // Ask the user which project to use
+    // Save the config file 
+    // Return the Config struct
+
+}
 #[cfg(test)]
 mod tests{
     use crate::cli_storage::{self, LocalStorage};
